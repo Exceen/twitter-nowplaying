ed17e06669c98fe64fb2a5fc80c4a347fb818fc2
Exceen <d11001@me.com>
2013-07-04 11:22:11 +0200

    Update #nowplaying.py
    "twitter" uses the Twitter API 1.0, so I switched to Tweepy

--- #nowplaying.py
+++ #nowplaying.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python
 from ScriptingBridge import *
-import twitter
+import tweepy
 
 iTunes = SBApplication.applicationWithBundleIdentifier_("com.apple.iTunes")
 curTrack = iTunes.currentTrack()
 
-api = twitter.Api(consumer_key='consumer_key',
-  				consumer_secret='consumer_secret',
-					access_token_key='access_token_key',
-					access_token_secret='access_token_secret')
-user = api.VerifyCredentials().GetScreenName()
-
-tweet = '#nowplaying ' + curTrack.artist() + ' - ' + curTrack.name() #+ ' - ' + curTrack.album()
-statuses = api.GetUserTimeline(user)
+auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
+auth.set_access_token('access_token_key', 'access_token_secret')
+api = tweepy.API(auth)
 
 if iTunes.isRunning() and iTunes.playerState() == 1800426320: # == 1800426352 when paused
+	try:
+		tweet = '#nowplaying ' + curTrack.artist() + ' - ' + curTrack.name() #+ ' - ' + curTrack.album()
+		api.update_status(tweet)
+		print api.me().name + ": " + tweet
+	except tweepy.error.TweepError, err:
+		print err
 
-	retry = True
-	loop_count = 0
-	
-	while retry:
-		try:
-			api.PostUpdate(tweet)
-			print user + ": " + tweet
-			retry = False
-		except twitter.TwitterError, err:
-			print err
-			retry = False
-		except:
-			retry = False
 
-		loop_count += 1
-		if loop_count > 9:
-			retry = False
+
